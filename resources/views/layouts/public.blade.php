d81dc989972ba5481b9ec9a53cb00baf2070e3cc
Mesbaul Islam <mesbaulislam778@gmail.com>
2026-07-17 20:43:50 +0600

    style: polish validation errors, empty states, and confirmations

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -43,6 +43,15 @@
         @if (session('error'))
             <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul class="mb-0">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         @yield('content')
     </main>
 
